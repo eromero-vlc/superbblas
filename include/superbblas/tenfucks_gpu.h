5228e089e225d6931a5fadf65be726a69e7f0634
Eloy Romero <eromero@jlab.org>
2025-10-09 22:51:37 -0400

    More fixes in the bsr kron kernel and add tests

--- include/superbblas/tenfucks_gpu.h
+++ include/superbblas/tenfucks_gpu.h
@@ -186,24 +186,24 @@ namespace superbblas {
                     int s_dir = (4 * dir + x_spin) * 2;
                     const double s_r = perm_scalars[s_dir], s_i = perm_scalars[s_dir + 1];
                     if (!x_is_zero) x_val_r = x[x_idx], x_val_i = x[x_idx + 1];
-                    x_val_r = x_val_r * s_r - x_val_i * s_i;
-                    x_val_i = x_val_r * s_i + x_val_i * s_r;
+                    const double b_val_r = x_val_r * s_r - x_val_i * s_i;
+                    const double b_val_i = x_val_r * s_i + x_val_i * s_r;
 
                     // a[real] times x[real] -> y[real]
                     y_val_r =
-                        __builtin_amdgcn_mfma_f64_4x4x4f64(a_val_r, x_val_r, y_val_r, 0, 0, 0);
+                        __builtin_amdgcn_mfma_f64_4x4x4f64(a_val_r, b_val_r, y_val_r, 0, 0, 0);
 
                     // a[real] times x[imag] -> y[imag]
                     y_val_i =
-                        __builtin_amdgcn_mfma_f64_4x4x4f64(a_val_r, x_val_i, y_val_i, 0, 0, 0);
+                        __builtin_amdgcn_mfma_f64_4x4x4f64(a_val_r, b_val_i, y_val_i, 0, 0, 0);
 
                     // a[imag] times x[real] -> y[imag]
                     y_val_i =
-                        __builtin_amdgcn_mfma_f64_4x4x4f64(a_val_i, x_val_r, y_val_i, 0, 0, 0);
+                        __builtin_amdgcn_mfma_f64_4x4x4f64(a_val_i, b_val_r, y_val_i, 0, 0, 0);
 
                     // a[imag] times x[imag] -> y[real]
                     y_val_r =
-                        __builtin_amdgcn_mfma_f64_4x4x4f64(-a_val_i, x_val_i, y_val_r, 0, 0, 0);
+                        __builtin_amdgcn_mfma_f64_4x4x4f64(-a_val_i, b_val_i, y_val_r, 0, 0, 0);
                 }
                 bool y_is_zero = (y_color == 3 || col >= ncols);
                 int y_idx = get_xy_idx_complex(ldy, ncols, y_color, y_spin, blk_row, col);
@@ -277,24 +277,24 @@ namespace superbblas {
                         int s_dir = (4 * dir + x_spin) * 2;
                         const float s_r = perm_scalars[s_dir], s_i = perm_scalars[s_dir + 1];
                         if (!x_is_zero) x_val_r = x[x_idx], x_val_i = x[x_idx + 1];
-                        x_val_r = x_val_r * s_r - x_val_i * s_i;
-                        x_val_i = x_val_r * s_i + x_val_i * s_r;
+                        const float b_val_r = x_val_r * s_r - x_val_i * s_i;
+                        const float b_val_i = x_val_r * s_i + x_val_i * s_r;
 
                         // a[real] times x[real] -> y[real]
                         y_val_r =
-                            __builtin_amdgcn_mfma_f32_4x4x1f32(a_val_r, x_val_r, y_val_r, 0, 0, 0);
+                            __builtin_amdgcn_mfma_f32_4x4x1f32(a_val_r, b_val_r, y_val_r, 0, 0, 0);
 
                         // a[real] times x[imag] -> y[imag]
                         y_val_i =
-                            __builtin_amdgcn_mfma_f32_4x4x1f32(a_val_r, x_val_i, y_val_i, 0, 0, 0);
+                            __builtin_amdgcn_mfma_f32_4x4x1f32(a_val_r, b_val_i, y_val_i, 0, 0, 0);
 
                         // a[imag] times x[real] -> y[imag]
                         y_val_i =
-                            __builtin_amdgcn_mfma_f32_4x4x1f32(a_val_i, x_val_r, y_val_i, 0, 0, 0);
+                            __builtin_amdgcn_mfma_f32_4x4x1f32(a_val_i, b_val_r, y_val_i, 0, 0, 0);
 
                         // a[imag] times x[imag] -> y[real]
                         y_val_r =
-                            __builtin_amdgcn_mfma_f32_4x4x1f32(-a_val_i, x_val_i, y_val_r, 0, 0, 0);
+                            __builtin_amdgcn_mfma_f32_4x4x1f32(-a_val_i, b_val_i, y_val_r, 0, 0, 0);
                     }
                 }
                 bool y_is_zero = (col >= ncols);
@@ -441,7 +441,7 @@ namespace superbblas {
             static dim3 block_size() { return dim3(32, 1, 1); }
 
             static dim3 grid_size(int block_rows, int num_cols) {
-                return dim3(block_rows, (num_cols + 3) / 4, 1);
+                return dim3(block_rows, num_cols, 1);
             }
         };
 
@@ -455,6 +455,31 @@ namespace superbblas {
 #        endif
         }
 
+        /// Computes the BSR-kron matrix vector multiplication
+        /// \param a: a[r*a_ldr+c*a_ldc+j*9] is the nonzero complex value at row r+3*I and column c+jj[j]
+        /// \param a_ldr: jump to the element on the next row within a nonzero block
+        /// \param a_ldc: jump to the element on the next column within a nonzero block
+        /// \param jj: column indices for each nonzero block
+        /// \param num_dirs: number of nonzero blocks per row
+        /// \param perm_scalars: 4*num_dirs with the nonzero complex values of the CSR kron matrices
+        /// \param perm: 4*num_dirs with the column indices of the CSR kron matrices
+        /// \param x: right-hand-side complex nonzeros with ordering 4,column,3,row
+        /// \param ldx: leading dimension of x
+        /// \param y: output complex nonzeros with ordering 4,column,3,row
+        /// \param ldy: leading dimension of y
+        /// \param ncols: number of columns on x and y
+	///
+	/// NOTE:
+	/// The intrinsic mma.sync.aligned.m8n8k4.row.col.f64.f64.f64.f64 performs the matrix-matrix
+	/// multiplication of a 8-by-4 and a 4-by-8 matrix on a single warp of 32 threads. The layout of
+	/// of the input and output matrix can be check here: 
+	///  https://docs.nvidia.com/cuda/parallel-thread-execution/index.html#warp-level-matrix-fragment-mma-884-f64
+	/// The thread with index i passes and receives the following values for doing A*B + C -> D:
+	/// - a(i/4,i%4)
+	/// - b(i%4,i/4)
+	/// - {c(i/4,(i%4)*2), c(i/4,(i%4)*2+1)}
+	/// - {d(i/4,(i%4)*2), d(i/4,(i%4)*2+1)}
+
         template <>
         inline __global__ void bsr_kron_3x3_4x4perm_kernel_fun<std::complex<double>>(
             const double *a, int a_ldr, int a_ldc, int *jj, int block_rows, int num_dirs,
@@ -464,7 +489,7 @@ namespace superbblas {
             (void)block_rows;
             double c[2] = {0, 0}; ///< accumulator
             double d[2] = {0, 0}; ///< result
-            auto col = blockIdx.y * 4 + (threadIdx.x / 4) % 4;
+            auto col = blockIdx.y;
             auto blk_row = blockIdx.x;
             auto a_row = (threadIdx.x / 4) % 4;
             auto a_col = threadIdx.x % 4;
@@ -472,9 +497,9 @@ namespace superbblas {
             auto x_spin = (threadIdx.x / 4) % 4;
             auto y_color = (threadIdx.x / 4) % 4;
             auto y_spin = (threadIdx.x % 2) * 2;
-            int a_re = (threadIdx.x / 16);    ///< 0:real, 1:imag
-            int x_re = (threadIdx.x % 8) / 4; ///< 0:real, 1:imag
-            int y_re0 = (threadIdx.x / 4) / 4;
+            int a_re = threadIdx.x / 16; ///< 0:real, 1:imag
+            int x_re = threadIdx.x / 16; ///< 0:real, 1:imag
+            int y_re0 = threadIdx.x / 16;
             int y_re1 = (threadIdx.x % 4) / 2;
             for (int dir = 0; dir < num_dirs; ++dir) {
                 // read a
@@ -491,9 +516,9 @@ namespace superbblas {
                 int s_dir = (4 * dir + x_spin) * 2;
                 const double s_r = perm_scalars[s_dir], s_i = perm_scalars[s_dir + 1];
                 if (!x_is_zero) x_val_r = x[x_idx], x_val_i = x[x_idx + 1];
-                x_val_r = x_val_r * s_r - x_val_i * s_i;
-                x_val_i = x_val_r * s_i + x_val_i * s_r;
-                const double x_val = x_re == 0 ? x_val_r : x_val_i;
+                const double b_val_r = x_val_r * s_r - x_val_i * s_i;
+                const double b_val_i = x_val_r * s_i + x_val_i * s_r;
+                const double b_val = x_re == 0 ? b_val_r : b_val_i;
 
                 // Use MMA intrinsic for matrix multiplication D = A*B + C
                 c[0] = d[0];
@@ -504,7 +529,7 @@ namespace superbblas {
                              "{%3}, "
                              "{%4, %5};"
                              : "=d"(d[0]), "=d"(d[1])
-                             : "d"(a_val), "d"(x_val), "d"(c[0]), "d"(c[1]));
+                             : "d"(a_val), "d"(b_val), "d"(c[0]), "d"(c[1]));
             }
             bool y_is_zero = (y_color == 3 || col >= ncols);
             int y_idx0 = get_xy_idx_complex(ldy, ncols, y_color, y_spin, blk_row, col);
@@ -514,22 +539,16 @@ namespace superbblas {
             // Then,
             // - threads y_re0 == 0 && y_re1 == 0 pass d[1] and get d[0] from y_re0 == 1 && y_re1 == 1
             // - threads y_re0 == 1 && y_re1 == 1 pass d[0] and get d[1] from y_re0 == 0 && y_re1 == 0
-            int source_thr = 0; // get from thread
-            int di = 0;         // either pass d[0] or d[1]
-            if (y_re0 == 0 && y_re1 == 0)
-                source_thr = (y_color + 4) * 4 + 2 + y_spin / 2, di = 1;
-            else if (y_re0 == 1 && y_re1 == 1)
-                source_thr = (y_color + 4) + y_spin / 2, di = 0;
-            else if (y_re0 == 0 && y_re1 == 1)
-                source_thr = (y_color + 4) * 4 + y_spin / 2, di = 1;
-            else /* if (y_re0 == 1 && y_re1 == 0) */
-                source_thr = (y_color + 4) + 2 + y_spin / 2, di = 0;
+            // - threads y_re0 == 0 && y_re1 == 1 pass d[1] and get d[0] from y_re0 == 1 && y_re1 == 0
+            // - threads y_re0 == 1 && y_re1 == 0 pass d[0] and get d[1] from y_re0 == 0 && y_re1 == 1
+            int source_thr = (y_color + 4 * (1 - y_re0)) * 4 + y_spin / 2 + 2 * (1 - y_re1);
+            int di = (y_re0 == y_re1 ? 1 - y_re0 : y_re1);
             const double d_other = __longlong_as_double(
                 __shfl_sync(0xffffffff, __double_as_longlong(d[di]), source_thr));
-            if (!y_is_zero && y_re0 == 0 && y_re1 == 0) y[y_idx0] = d[0] + d_other;
-            if (!y_is_zero && y_re0 == 1 && y_re1 == 1) y[y_idx1] = d[1] + d_other;
+            if (!y_is_zero && y_re0 == 0 && y_re1 == 0) y[y_idx0] = d[0] - d_other;
+            if (!y_is_zero && y_re0 == 1 && y_re1 == 1) y[y_idx1] = d_other - d[1];
             if (!y_is_zero && y_re0 == 0 && y_re1 == 1) y[y_idx0 + 1] = d[0] + d_other;
-            if (!y_is_zero && y_re0 == 1 && y_re1 == 0) y[y_idx1 + 1] = d[0] + d_other;
+            if (!y_is_zero && y_re0 == 1 && y_re1 == 0) y[y_idx1 + 1] = d[1] + d_other;
 #        else
             (void)a;
             (void)a_ldr;
@@ -673,8 +692,8 @@ namespace superbblas {
 
         /// Computes the BSR-kron matrix vector multiplication
         /// \param a: a[r*a_ldr+c*a_ldc+j*9] is the nonzero value at row r+3*I and column c+jj[j]
-        /// \param a_ldr: jump to the element at the next row in a nonzero block
-        /// \param a_ldc: jump to the element at the next column in a nonzero block
+        /// \param a_ldr: jump to the element on the next row within a nonzero block
+        /// \param a_ldc: jump to the element on the next column within a nonzero block
         /// \param jj: column indices for each nonzero block
         /// \param num_dirs: number of nonzero blocks per row
         /// \param perm_scalars: 4*num_dirs with the nonzero values of the CSR kron matrices
@@ -688,7 +707,7 @@ namespace superbblas {
         ///
         /// NOTE:
         /// The routine does:
-        ///   y(0:3,0:2,I,n) = \sum_{j=0:dirs-1} [ a(0:2,I,0:2,J(I,j)) \Kron_Prod
+        ///   y(0:3,0:2,I,n) = \sum_{j=0:dirs-1} [ a(0:2,I,0:2,J(I,j)) \kron
         ///                                          k(0:4,0:4,j)  ] * x(0:3,0:2,J(I,j),n),
         /// where
         /// - J(I,j) is the block column index for block row I and direction j;
@@ -698,6 +717,13 @@ namespace superbblas {
         /// - x,y are the input/output tensors with several fields indexed by `n`.
         ///
         /// The input and output tensors are ordered as a kind of row-major: (0:3,n,0:2,I).
+        ///
+        /// The routine performs the product of the matrix-vector products where the matrix is the Kronecker
+        /// product of a 3-by-3 and a 4-by-4 matrices as two matrix-matrix products using the "vec-trick":
+        /// if d = (A \kron B) * c, for m-by-m A and n-by-n B matrices, then
+        ///   d = vec(B*mat(c)*A^T),
+        /// where mat(x) reshapes x into a n-by-m matrix, and vec(X) returns a column vector by stacking the
+        /// columns of X.
 
         template <typename T>
         DECL_BSR_KRON_3x3_4x4PERM_T(void bsr_kron_3x3_4x4perm(const T *a, int a_ldr, int a_ldc,
--- tests/tenfucks.cpp
+++ tests/tenfucks.cpp
@@ -1,5 +1,4 @@
 #include "superbblas.h"
-#include "superbblas/tenfucks_cpu.h"
 #include <algorithm>
 #include <ccomplex>
 #include <chrono>
@@ -64,11 +63,74 @@ template <typename SCALAR> void test() {
     }
 }
 
+template <typename SCALAR> void test_gpu(const Gpu &xpu) {
+    const std::unordered_map<std::type_index, std::string> type_to_string{
+        {std::type_index(typeid(std::complex<float>)), "complex float"},
+        {std::type_index(typeid(std::complex<double>)), "complex double"}};
+
+    const bool specialized_kernel_support =
+        superbblas::detail::available_bsr_kron_3x3_4x4perm<SCALAR>(xpu);
+    if (!specialized_kernel_support) {
+        std::cout << "Not doing testing for " << type_to_string.at(std::type_index(typeid(SCALAR)))
+                  << std::endl;
+        return;
+    }
+    std::cout << "Testing " << type_to_string.at(std::type_index(typeid(SCALAR)))
+              << " with a specialized kernel support" << std::endl;
+
+    vector<SCALAR, Cpu> a_cpu(9, Cpu{});
+    for (size_t i = 0; i < a_cpu.size(); ++i) a_cpu[i] = {i * 1.f, .5f * i};
+    auto a = makeSure(a_cpu, xpu);
+    vector<SCALAR, Cpu> k_cpu(4, Cpu{});
+    for (size_t i = 0; i < k_cpu.size(); ++i) k_cpu[i] = {.5f * (i + 1), 1.f};
+    auto k = makeSure(k_cpu, xpu);
+    vector<int, Cpu> k_perm_cpu(4, Cpu{});
+    for (size_t i = 0; i < k_perm_cpu.size(); ++i) k_perm_cpu[i] = 3 - i;
+    auto k_perm = makeSure(k_perm_cpu, xpu);
+    vector<int, Cpu> jj_cpu(1, Cpu{});
+    jj_cpu[0] = 0;
+    auto jj = makeSure(jj_cpu, xpu);
+
+    for (int n = 1; n <= 10; ++n) {
+        std::cout << ".. for rhs= " << n << std::endl;
+        vector<SCALAR, Cpu> x_cpu(12 * n, Cpu{});
+        for (size_t i = 0; i < x_cpu.size(); ++i) x_cpu[i] = {i * 1.f, .1f * i};
+        auto x = makeSure(x_cpu, xpu);
+
+        {
+            vector<SCALAR, Gpu> y(12 * n, xpu);
+            bsr_kron_3x3_4x4perm(a.data(), 1, 3, jj.data(), 1, 1, k.data(), k_perm.data(), x.data(),
+                                 12 * n, y.data(), 12 * n, n, xpu);
+            auto y_cpu = makeSure(y, Cpu{});
+
+            std::vector<SCALAR> y0(12 * n);
+            for (int j = 0; j < n; j++)
+                for (int k = 0; k < 4; ++k)
+                    for (int i = 0; i < 3; ++i)
+                        for (int s = 0; s < 3; ++s)
+                            y0[k + 4 * j + 4 * n * i] += a_cpu[i + 3 * s] *
+                                                         x_cpu[k_perm_cpu[k] + 4 * j + 4 * n * s] *
+                                                         k_cpu[k];
+
+            double r = 0;
+            for (int i = 0; i < 12 * n; ++i) r += std::norm(y0[i] - y_cpu[i]);
+            std::cout << "Error: " << std::sqrt(r) << std::endl;
+        }
+    }
+}
+
 int main(int, char **) {
 #ifdef SUPERBBLAS_USE_FLOAT16
     test<std::complex<_Float16>>();
 #endif
     test<std::complex<float>>();
     test<std::complex<double>>();
+#ifdef SUPERBBLAS_USE_GPU
+    {
+        Context ctx = createGpuContext(0);
+        test_gpu<std::complex<double>>(ctx.toGpu(0));
+    }
+#endif
+
     return 0;
 }
